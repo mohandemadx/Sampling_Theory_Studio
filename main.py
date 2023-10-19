89bd04cd2282f89d7ad260e4bf17efcee2d06f9f
Mohand Emad Attya <102548631+mohandemadx@users.noreply.github.com>
2023-10-19 05:33:24 +0300

    Yala beena n3ml sho8l moot

--- main.py
+++ main.py
@@ -1,17 +1,64 @@
-# This is a sample Python script.
+import os
+import sys
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+import numpy as np
+from PyQt5.QtWidgets import QMainWindow, QPushButton, QApplication, QFileDialog, QListWidgetItem
+from PyQt5.uic import loadUiType
+from os import path
+
+from PyQt5.uic.properties import QtCore
+
+FORM_CLASS, _ = loadUiType(path.join(path.dirname(__file__), "task2_design.ui"))
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+class Signal:
+    def __init__(self, file_name, file_path, data):
+        self.name = file_name
+        self.path = file_path
+        self.data = data
 
-ahmed = 'l0l'
+    def __str__(self):
+        return f"Signal: {self.name}, Path: {self.path}, Frequency: {self.frequency}, Data: {self.data}"
 
-# Press the green button in the gutter to run the script.
+
+class MainApp(QMainWindow, FORM_CLASS):
+    def __init__(self, parent=None):
+        super(MainApp, self).__init__(parent)
+        QMainWindow.__init__(self)
+        self.setupUi(self)
+
+        # Variables
+        self.importedSignals = []
+        self.uploadButton.clicked.connect(self.upload_file)
+
+    def upload_file(self):
+
+        options = QFileDialog.Options()
+        options |= QFileDialog.ReadOnly
+
+        files, _ = QFileDialog.getOpenFileNames(
+            self, "QFileDialog.getOpenFileNames()", "", "All Files (*);;Text Files (*.txt)", options=options)
+
+        if files:
+            for file in files:
+                # Store file path
+                file_name = os.path.basename(file)
+                data = np.fromfile(file, dtype=np.int16)
+                signal = Signal(file_name, file, data)
+
+                self.importedSignals.append(signal)
+                if self.importedSignals:
+                    signal.__str__()
+
+
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainApp()
+    window.show()
+    app.exec_()
+
+
 if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    main()
